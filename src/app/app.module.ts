5c6889873922cbb05eed82723d8c1d17869d01a3
dimaxdqwerty <dimaxdqwerty@gmail.com>
2022-07-28 09:09:13 +0300

    delete unused directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChessBoardComponent } from './chess-board/chess-board.component';
-import { DragPieceDirective } from './directives/drag-piece.directive';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChessBoardComponent,
-    DragPieceDirective
   ],
   imports: [
     BrowserModule,
